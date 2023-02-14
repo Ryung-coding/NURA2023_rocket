0990a6977e6797a1f0967a69a0d76f61ce5ab687
Ryung <ryung9514@naver.com>
2023-02-14 18:40:30 +0900

    mpu6050 예제 / yaw방향의 자이로값만 비주얼하는 것으로 써놓음 / LPF 적용한 경우

--- rocket/mpu6050_example/mpu6050_example.ino
+++ rocket/mpu6050_example/mpu6050_example.ino
@@ -1,75 +1,48 @@
 #include<Wire.h>
 
+//SCL = A5 SDA = A4
+
 const int MPU=0x68;//MPU6050 I2C주소
 
 int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
-void get6050();
 
- 
+void get6050()
+{
+  Wire.beginTransmission(MPU);//MPU6050 호출
+  Wire.write(0x3B);//AcX 레지스터 위치 요청
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,14,true);//14byte의 데이터를 요청
+  
+  AcX=Wire.read()<<8|Wire.read();//두개의 나뉘어진 바이트를 하나로 이어붙입니다.
+  AcY=Wire.read()<<8|Wire.read();
+  AcZ=Wire.read()<<8|Wire.read();
+  Tmp=Wire.read()<<8|Wire.read();
+  GyX=Wire.read()<<8|Wire.read();
+  GyY=Wire.read()<<8|Wire.read();
+  GyZ=Wire.read()<<8|Wire.read();
+}
+
 
 void setup()
-
 {
-
   Wire.begin();
-
   Wire.beginTransmission(MPU);
-
   Wire.write(0x6B);
-
   Wire.write(0);//MPU6050 을 동작 대기 모드로 변경
-
   Wire.endTransmission(true);
-
   Serial.begin(9600);
-
+  pinMode(9, OUTPUT);
 }
 
 void loop()
-
 {
-
-  get6050();//센서값 갱신
-
-  //받아온 센서값을 출력합니다.
-
-  Serial.print(AcX);
-
-  Serial.print("");
-
-  Serial.print(AcY);
-
-  Serial.print("");
-
-  Serial.print(AcZ);
-
-  Serial.println();
-
+  get6050();
+  
+  filter = 0.1*GyZ + 0.9*past;  //LPF
+  Serial.print(GyZ);
+  Serial.print(",");
+  Serial.println(filter);
+  past = filter;
   delay(15);
-
 }
-
-void get6050(){
-
-  Wire.beginTransmission(MPU);//MPU6050 호출
-
-  Wire.write(0x3B);//AcX 레지스터 위치 요청
-
-  Wire.endTransmission(false);
-
-  Wire.requestFrom(MPU,14,true);//14byte의 데이터를 요청
-
-  AcX=Wire.read()<<8|Wire.read();//두개의 나뉘어진 바이트를 하나로 이어붙입니다.
-
-  AcY=Wire.read()<<8|Wire.read();
-
-  AcZ=Wire.read()<<8|Wire.read();
-
-  Tmp=Wire.read()<<8|Wire.read();
-
-  GyX=Wire.read()<<8|Wire.read();
-
-  GyY=Wire.read()<<8|Wire.read();
-
-  GyZ=Wire.read()<<8|Wire.read();
