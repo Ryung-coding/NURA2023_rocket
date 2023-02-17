a5ba0a8cd86684045f4228586327426c8cbf0f2a
Ryung <ryung9514@naver.com>
2023-02-17 20:02:02 +0900

    BLDC 파일로 모터 작동가능 / Serial 값으로 duty조절가능 / i2c 통신 주소 확인하는 파일로 여러 isc 센서 퓨전 가능해짐

--- rocket/moment wheel/BLDC/BLDC.ino
+++ rocket/moment wheel/BLDC/BLDC.ino
@@ -1,52 +1,46 @@
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_PWMServoDriver.h> //scl = A5 / sda = A4
 
 Adafruit_PWMServoDriver pwm=Adafruit_PWMServoDriver();
 
-int duty = 0; //(%) 1000~2000us 
-int motor_speed = 0;
+float duty = 0; // 0~100 [%]
+float motor_speed, motor_speed_past, motor_speed_filter; //1000~2000us 
+
+float lowpassfilter(float filter, float data, float lowpass_constant)
+{
+  filter = data * (1 - lowpass_constant) + filter * lowpass_constant;
+  return filter;   
+}
+
+void BLDC()
+{
+  do
+  {
+    motor_speed = constrain(map(duty, 0, 100, 1613, 3226), 1613, 3226); //1000~2000ms 로 제한을 걸고 duty를 mapping 하여 변환함
+    motor_speed_filter = lowpassfilter(motor_speed_past, motor_speed, 0.9);
+    Serial.println(motor_speed_filter);
+    pwm.setPWM(0,0,motor_speed_filter);
+    motor_speed_past = motor_speed_filter;
+    delay(20);
+    }while(abs(motor_speed - motor_speed_filter) > 3);
+    //delay(1000);
+    //pwm.setPWM(0,0,1613);
+}
+
 
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(4,INPUT);
   pwm.begin();
-  pwm.setPWMFreq(380); //->400hz로 측정됨 = 2.5ms
-  Serial.println("setting time");
-  
-  pwm.setPWM(0,0,3225); //1당 0.620us로 계산됨, 2000us
-  delay(1000);
-  Serial.println("HIGH set");
-  pwm.setPWM(0,0,1613); //1당 0.620us 1000us
-  delay(1000);
-  Serial.println("LOW set");
-  
+  pwm.setPWMFreq(380); //->400hz로 측정됨 = 2.5ms 즉 1당 0.620us
 }
 
 void loop() 
 {
-  //int PWM = constrain(map(duty, 0, 100, 0, ), 0, 4032); 
-  for (motor_speed = 1613; motor_speed<1935;motor_speed+=300)// 1000us에서 1935us 까지 증가
+  if(Serial.available())
   {
-     pwm.setPWM(0,0,motor_speed); //1당 0.620us
-    delay(30);
-    Serial.print(motor_speed);
-    Serial.print(" = ");
-    Serial.println(map(motor_speed, 1613, 3225, 0, 100));
-    pwm.setPWM(0,0,0);
-    delay(100);
+    duty = Serial.parseInt();
+    BLDC();
   }
-
-    for (motor_speed = 1935; motor_speed>1613;motor_speed-=300)
-  {
-     pwm.setPWM(0,0,motor_speed); //1당 0.620us
-    delay(30);
-    Serial.print(motor_speed);
-    Serial.print(" = ");
-    Serial.println(map(motor_speed, 1613, 3225, 0, 100));
-    pwm.setPWM(0,0,0);
-    delay(100);
-  }
-
 
 }
