f2e40c4348cb1aadb95c431290deec598718ec41
Daejung <minerva01st@gmail.com>
2023-04-25 00:57:05 +0900

    XBee / BMP280, DHT22 추가

--- rocket/Sensing_data & X-bee/XBee/Rocket_Mega_Sensor/Rocket_Mega_Sensor.ino
+++ rocket/Sensing_data & X-bee/XBee/Rocket_Mega_Sensor/Rocket_Mega_Sensor.ino
@@ -1,36 +1,60 @@
-// 회로 연결(MEGA 기준)
-// Rocket_MEGA : 0(RX0) - 18(TX1) , 1(TX0) - 19(RX1) (Rocket_MEGA_SENSOR의 0 1을 Rocket_MEGA의 18 19와 연결)
-// IMU : SCL - SCL , SDA - SDA
-// GPS : TXD - RX1(19) , RXD - TX1(20) 
-// MicroSD : MISO 50, MOSI 51, SCK 52, SC 53
+// 회로 연결(MEGA 기준) 
+// Rocket_MEGA : 0(RX0) - 18(TX1), 1(TX0) - 19(RX1) (Rocket_MEGA_SENSOR의 0 1을 Rocket_MEGA의 18 19와 연결)
+// BMP280 : SCL - 21(SCL), SDA - 20(SDA)
+// DHT22  : SIG - 2
+// IMU    : SCL - SCL, SDA - SDA
+// GPS    : TXD - 19(RX1), RXD - 20(TX1) 
+// MicroSD : MISO - 50, MOSI - 51, SCK - 52, SC - 53
 
-#include <SPI.h>
-#include <SD.h>
-#include <Wire.h>
-#include <SoftwareSerial.h>
-#include <TinyGPS.h>
-#define LEN_OF_SENSOR_ARRAY 10 // 센서값 배열의 길이
 
-int16_t ax, ay, az, gx, gy, gz;  // 가속도계 및 자이로스코프 값 저장 변수
+#include <Wire.h>               // I2C 통신용 라이브러리
+#include <Adafruit_BMP280.h>    // BMP280 라이브러리
+#include <DHT.h>                // DHT22 라이브러리
+#include <TinyGPS.h>            // GPS 라이브러리
+#include <SD.h>                 // MicroSDCard 라이브러리
+#define LEN_OF_SENSOR_ARRAY 14  // 센서 값 배열의 길이
 
-TinyGPS gps;  // GPS 객체
 
-File myFile;  // MicroSDCard 객체
+Adafruit_BMP280 bmp;  // BMP280 객체
+DHT dht(2, DHT22);    // DHT22 객체
+TinyGPS gps;          // GPS 객체
+File myFile;          // MicroSDCard 객체
+
+double sensorData[LEN_OF_SENSOR_ARRAY];     // 센서 값 배열
+
+double Pressure, Altitude;                  // 기압, 고도
+double Temperature, Humidity;               // 온도, 습도
+int16_t ax, ay, az, gx, gy, gz;             // 3축 가속도, 각속도
+float flat, flon, speedMPS, courseDegree;  // 위도, 경도, 속도, 각도
+unsigned long age;                          // GPS 정확도
+
 
 void setup() {
   Serial.begin(2400);
   Wire.begin();
-  Serial1.begin(9600);  // GPS 보드레이트는 9600으로 고정 , TX : 19  RX : 18
-  initializeMicroSD();  // MicroSDCard 초기화
-}
+  bmp.begin(0x76);
+  dht.begin();
+  Serial1.begin(9600);  // GPS 보드레이트는 9600으로 고정
+  initializeMicroSD();  
+}// Setup End
+
 
 void loop() {
+//  BMP280에서 기압, 고도 값 읽어오기
+  Pressure = bmp.readPressure()/1000;
+  Altitude = bmp.readAltitude(1019.1);
+
+
+// DHT22에서 온도, 습도 값 읽어오기
+  Temperature = dht.readTemperature();
+  Humidity = dht.readHumidity();
   
-//   MPU6050에서 가속도계 및 자이로스코프 값 읽어오기
+  
+//  MPU9250에서 3축 가속도, 각속도 값 읽어오기
   Wire.beginTransmission(0x68);
-  Wire.write(0x3B);  // 시작 주소 0x3B (ACCEL_XOUT_H)
+  Wire.write(0x3B);                     // 시작 주소 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(0x68, 14, true);  // 총 14개 레지스터 요청
+  Wire.requestFrom(0x68, 14, true);     // 총 14개 레지스터 요청
   ax = Wire.read() << 8 | Wire.read();  // X축 가속도계 값 읽어오기
   ay = Wire.read() << 8 | Wire.read();  // Y축 가속도계 값 읽어오기
   az = Wire.read() << 8 | Wire.read();  // Z축 가속도계 값 읽어오기
@@ -41,42 +65,46 @@ void loop() {
   gz = Wire.read() << 8 | Wire.read();  // Z축 자이로스코프 값 읽어오기
 
 
-//  GPS 센서로 위도, 경도, 속도, 각도 읽어오기
-  float flat, flon, speedMPS, courseDegree;   // 위도, 경도, 속도, 각도
-  unsigned long age;  // 정확도
-  
+//  GPS 센서에서 위도, 경도, 속도, 각도 값 읽어오기
   gps.f_get_position(&flat, &flon, &age);   // 위도, 경도, 정확도 얻음
   speedMPS = gps.f_speed_mps();             // 속도 얻음
   courseDegree = gps.f_course();            // 각도 얻음
 
 
-//   센서 값 배열 만들기
-  int sensorData[LEN_OF_SENSOR_ARRAY];
-  sensorData[0] = ax;
-  sensorData[1] = ay;
-  sensorData[2] = az;
-  sensorData[3] = gx;
-  sensorData[4] = gy;
-  sensorData[5] = gz;
-  sensorData[6] = flat;
-  sensorData[7] = flon;
-  sensorData[8] = speedMPS;
-  sensorData[9] = courseDegree;
+//  배열에 센서 값 저장하기
+  sensorData[0] = Pressure;
+  sensorData[1] = Altitude;
+  sensorData[2] = Temperature;
+  sensorData[3] = Humidity;
+  sensorData[4] = ax;
+  sensorData[5] = ay;
+  sensorData[6] = az;
+  sensorData[7] = gx;
+  sensorData[8] = gy;
+  sensorData[9] = gz;
+  sensorData[10] = flat;
+  sensorData[11] = flon;
+  sensorData[12] = speedMPS;
+  sensorData[13] = courseDegree;
 
 
-//   시리얼 통신으로 센서 값 배열 보내기, MicroSD카드에 입력하기
+//  시리얼 통신으로 센서 값 배열 보내기, MicroSD에 입력하기
   for (int i = 0; i < LEN_OF_SENSOR_ARRAY; i++){
     Serial.print(char('a' + i));
     Serial.print(sensorData[i]);
     writeValue(String(sensorData[i]));
     writeValue(",");
   }
+  Serial.println();
   writeValue("\n");
   smartdelay(100);
-} // Loop End
+}// Loop End
 
 
-static void smartdelay(unsigned long ms){   // GPS용 딜레이
+
+
+// GPS용 딜레이
+static void smartdelay(unsigned long ms){ 
   unsigned long start = millis();
   do 
   {
@@ -86,10 +114,11 @@ static void smartdelay(unsigned long ms){   // GPS용 딜레이
 }
 
 
-void initializeMicroSD(){   // MicroSD카드 모듈을 초기화한다.
+// MicroSD 모듈을 초기화
+void initializeMicroSD(){   
   Serial.print("Initializing SD card...");
 
-  // 아두이노 메가 기준 MISO 50, MOSI 51, SCK 52, SC 53
+  // Mega 기준 MISO 50, MOSI 51, SCK 52, SC 53
   if (!SD.begin(53)) { // SD카드 모듈을 초기화합니다, 매개변수에 SC 핀 번호 입력
     Serial.println("initialization failed!"); // SD카드 모듈 초기화에 실패하면 에러를 출력합니다.
     while(1);
@@ -98,7 +127,8 @@ void initializeMicroSD(){   // MicroSD카드 모듈을 초기화한다.
 }
 
 
-void writeValue(String val){  // string 변수를 매개변수로 받아 MicroSD카드의 텍스트 파일에 작성한다.
+// string 변수를 매개변수로 받아 MicroSD의 txt 파일에 작성
+void writeValue(String val){  
   // 파일을 열어 쓸 준비를 합니다. 한 번에 하나의 파일만 열 수 있습니다.
   myFile = SD.open("value.txt", FILE_WRITE); // 두 번째 인자가 있으면 쓰기모드입니다.
 
@@ -112,7 +142,8 @@ void writeValue(String val){  // string 변수를 매개변수로 받아 MicroSD
 }
 
 
-void openValueFile(){   // MicroSD카드 안의 텍스트 파일 값을 읽어 Serial 출력한다.
+// MicroSD 안의 txt 파일 값을 읽어 Serial 출력
+void openValueFile(){   
   // 파일을 읽기 위해 다시 엽니다. 두 번째 인자가 없으면 읽기모드입니다.
   myFile = SD.open("value.txt");
   if (myFile) {
